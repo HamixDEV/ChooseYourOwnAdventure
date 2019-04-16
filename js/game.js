bf2b4e94edf500a81bc6c5d8d9e5c5b386bf85c9
Hamidou Nimaga <hamidou.nimaga@pascholars.org>
2019-04-16 21:15:56 +0000

    ok

--- js/game.js
+++ js/game.js
@@ -4,7 +4,6 @@ var game = {
     music: "98_Lost_Mine.mp3",
     background_image: "intro-bg.jpg",
     levels: {
-
         start: {
             message: "You come across a dark cave",
             choices: [
@@ -17,13 +16,47 @@ var game = {
                     text: "Keep on moving",
                     nextLevel: "field",
                 },
+                {
+                    text: "Slap Cave?",
+                    nextLevel: "slapcave",
+                }
+            ]
+        },
+         slapcave: {
+            background_image: "fire.gif",
+            music: "Final-Fantasy-7-Boss-Battle.mp3",
+            message: "'pls stop'",
+            choices: [
+                {
+                    text: "Stop?",
+                    nextLevel: "start",
+                },
+                {
+                    text: "Start Punching",
+                    nextLevel: "CaveMan",
+                },
             ]
         },
 
         cave: {
-            background_image: "fire.gif",
+            background_image: "orge.gif",
             music: "Final-Fantasy-7-Boss-Battle.mp3",
-            message: "You come across a fire monster or something!",
+            message: "You come across a big orge",
+            choices: [
+                {
+                    text: "Fight",
+                    nextLevel: "End",
+                },
+                  {
+                    text: "Run away!",
+                    nextLevel: "field",
+                },
+            ]
+        },
+         CaveMan: {
+            background_image: "punch.gif",
+            music: "Final-Fantasy-7-Boss-Battle.mp3",
+            message: "You come across a 'caveman' and get punched back",
             choices: [
                 {
                     text: "Start over",
@@ -32,13 +65,78 @@ var game = {
             ]
         },
 
+
         field: {
-            message: "Some adventurer you are...",
+             background_image: "burger.gif",
+            message: "You find a cheeseburger",
             choices: [
                 {
-                    text: "Start over",
-                    nextLevel: "start",
+                    text: "Eat it",
+                    nextLevel: "Victoryll",
                 },
+                    {
+                    text: "Take it with you",
+                    nextLevel: "Cavept2",
+                },
+            ]
+        },
+        Cavept2: {
+            background_image: "orge.gif",
+            music: "Final-Fantasy-7-Boss-Battle.mp3",
+            message: "You come across a big orge",
+            choices: [
+                {
+                    text: "Fight",
+                    nextLevel: "End11",
+                },
+                  {
+                    text: "Give cheeseburger",
+                    nextLevel: "Victory",
+                },
+            ]
+        },
+          Victory: {
+            background_image: "fire.gif",
+            music: "Final-Fantasy-7-Boss-Battle.mp3",
+            message: "He takes you our of the cave and shows you the way home",
+            choices: [
+                {
+                    text: "Yahh!! you win, play again pls",
+                    nextLevel: "start",
+                }
+            ]
+        },
+         Victoryll: {
+            background_image: "Eating.gif",
+            music: "Final-Fantasy-7-Boss-Battle.mp3",
+            message: "You're Famsihed now",
+            choices: [
+                {
+                    text: "Yahh!! you win, play again pls",
+                    nextLevel: "start",
+                }
+            ]
+        },
+         End: {
+            background_image: "fire.gif",
+            music: "Final-Fantasy-7-Boss-Battle.mp3",
+            message: "You're Defeated",
+            choices: [
+                {
+                    text: "You lose",
+                    nextLevel: "start",
+                }
+            ]
+        },
+        End11: {
+            background_image: "fire.gif",
+            music: "Final-Fantasy-7-Boss-Battle.mp3",
+            message: "You're Defeated and lose you're burger",
+            choices: [
+                {
+                    text: "You lose, play again",
+                    nextLevel: "start",
+                }
             ]
         },
 
